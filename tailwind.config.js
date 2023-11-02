93858926f161996533874ea4fc36deb8202bb806
Vlatko Stojkoski <stojvlatko@gmail.com>
2023-11-02 12:22:07 +0100

    fix up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./(dist|src)/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+			colors: {
+				accent: '#59b73a',
+				background: {
+					primary: '#101618',
+					secondary: '#223324',
+				},
+				foreground: {
+					primary: '#ffffff',
+					secondary: '#e8e8e8',
+				},
+			},
+		},
   },
   plugins: [],
 }
