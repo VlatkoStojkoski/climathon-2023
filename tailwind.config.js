dc58f3056b1fef9d975dbf661355000eddf6fc70
FilipGT2 <filipnajdovski091@gmail.com>
2023-11-02 11:28:40 +0100

    start-4

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./(dist|src)/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        geist: ['Geist', 'sans'],
+      },
+      colors: {
+          primary: '#59B73A' ,
+          secondary: {
+            light: '#223324',
+            dark: '#101618'
+          },
+      },
+    },
   },
   plugins: [],
 }
