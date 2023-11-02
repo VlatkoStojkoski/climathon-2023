12c3ee1e067d747b99f603dafc871990ece59be1
Ana Mladenova <mladenovaana6@gmail.com>
2023-11-03 00:13:57 +0100

    Routes Elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./(dist|src)/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: '#59b73a',
+        background: {
+          primary: '#101618',
+          secondary: '#223324',
+          navbar: '#76797A',
+        },
+        foreground: {
+          primary: '#ffffff',
+          secondary: '#e8e8e8',
+          navbar: '#DADBDB',
+          button: '#071302',
+          or: '#777D74',
+        },
+      },
+    },
   },
   plugins: [],
 }
